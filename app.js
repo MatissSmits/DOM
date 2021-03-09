b1cd37a47d1458c475123547187f8f87ff1a2e94
MatissSmits <78017254+MatissSmits@users.noreply.github.com>
2021-03-09 16:57:43 +0200

    DOM

---
+++ app.js
@@ -0,0 +1,71 @@
+//var titles = document.getElementsByClassName('title');
+
+//console.log(Array.isArray(titles));
+//console.log(Array.isArray(Array.from(titles)));
+
+//Array.from(titles).forEach(function(item){
+  //console.log(item);
+//})
+
+
+//const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(wmf);
+
+//var books = document.querySelectorAll('#book-list li .name');
+//console.log(books);
+
+//books = document.querySelectorAll('#book-list li .name');
+//console.log(books);
+
+//Array.from(books).forEach(function(book){
+    //console.log(book)
+    //book.textContent += '(book title)';
+//});
+
+//const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2>Books and more books...</h2>';
+//bookList.innerHTML += '<p>This is how you add HTML</p>';
+//const banner = document.querySelector('#page-banner');
+//console.log('#page-banner node type is:', banner.nodeType);
+//console.log('#page-banner node name is:', banner.nodeName);
+//console.log('#page-banner has child nodes:', banner.hasChildNodes());
+
+//const clonedBanner =banner.cloneNode(true);
+//console.log(clonedBanner);
+//const bookList = document.querySelector('#book-list');
+
+//console.log('the parent node is:', bookList.parentNode);
+//console.log('the parent element is:', bookList.parentElement.parentElement);
+
+//console.log(bookList.childNodes);
+
+//const bookList = document.querySelector('#book-list');
+
+//console.log('book-list next sibling is:', bookList.nextSibling);
+//console.log('book-list next sibling element is:', bookList.nextElementSibling);
+
+
+//console.log('book-list previous sibling is:', bookList.previousSibling);
+//console.log('book-list previous sibling element is:', bookList.previousElementSibling);
+
+//bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!';
+
+var btns = document.querySelectorAll('#book-list .delete');
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', dunction(e){
+
+        const li = e.target.parentElement;
+
+        li.parentNode.removeChild(li)
+
+
+    });
+}):
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to', e.target.textContent, 'was prevented');
+})
